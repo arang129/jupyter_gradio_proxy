efb678ac2741050af1918ce5e039a306fb77b872
arang129 <48246455+arang129@users.noreply.github.com>
2025-07-12 13:13:16 +0800

    Update and rename __init__.py to __init__.py

---
+++ jupyter_gradio_proxy/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+def setup_gradio_proxy():
+    """ Proxy wrapper to launch Streamlit from JupyterHub """
+
+    return {
+        'command': [
+            "python", "run", "/home/jupyter-data/webapp/gradio/gradio_app.py",
+        ],
+        'port':7860,
+        'absolute_url': False,
+        'timeout': 60,
+        'launcher_entry': {
+            'enabled': True,
+            'title': 'Gradio',
+            'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'notes.svg'),
+        }
+    }
